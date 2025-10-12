daf09e197110073748a411637acfb368bcf994e5
Nurbolottop <erk1nbaevw2711@gmail.com>
2025-10-12 23:25:04 +0600

    upds

--- app/core/project_settings/key.py
+++ app/core/project_settings/key.py
@@ -10,4 +10,4 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
